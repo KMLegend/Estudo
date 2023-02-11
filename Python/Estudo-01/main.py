eeedab0e95ddf80260590284f7f1370ab5aeac81
Kevin <kevinmaykelgyn@gmail.com>
2023-02-11 15:32:51 -0300

    Crição da primeira pasta de Estudo

---
+++ Python/Estudo-01/main.py
@@ -0,0 +1,8 @@
+# Quantidade de Laboratorio 5 --> com 20 maquinas cada.
+# Horario das 8 as 22 --> de segunda a sexta.
+# Tem que Ler o ID --> é a matricula.
+# Semanal --> só vale na semana que for selecionada.
+
+
+
+
