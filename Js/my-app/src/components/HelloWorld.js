3ab4fa96282e96ad5b1776e308078bc6d710868a
Kevin Maykel <kevinmaykelgyn@gmail.com>
2023-03-11 11:36:35 -0300

    Inicio dos estudos em react JS

---
+++ Js/my-app/src/components/HelloWorld.js
@@ -0,0 +1,15 @@
+import Bd from './Bd';
+
+function HelloWorld(){
+    
+    
+    return (
+        <div>
+            <h1>Teste no Banco de dados Retornou isso: </h1>
+            <Bd/>
+        </div>
+    );
+}
+
+
+export default HelloWorld
