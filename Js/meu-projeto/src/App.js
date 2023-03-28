c9556bc8d8280d22496b87e2b25c0e230c1bbe52
Kevin <kevinmaykelgyn@gmail.com>
2023-03-28 10:37:29 -0300

    Estudei CSS e Props

--- Js/meu-projeto/src/App.js
+++ Js/meu-projeto/src/App.js
@@ -1,14 +1,22 @@
-import HelloWorld from './component/HelloWorld';
-import SayMyName from './component/SayMyName';
 import './App.css';
-
+import HelloWorld from './component/HelloWorld';
+import Pessoa from './component/Pessoa';
+import SayMyName from './component/SayMyName';
+import Frase from './component/Frase';
 
 function App() {
+  const nome = "Maria"
   return (
     <div className="App">
-      <HelloWorld/>
-      <br></br>
+  
+      <Frase />
+      <Frase />
+      
       <SayMyName nome = "Kevin"/>
+      <SayMyName nome = "Aoba"/>
+      <SayMyName nome = {nome}/>
+      <Pessoa nome = "Kevin" idade = "18" profissao = "Programador" foto = "https://via.placeholder.com/150"/>
+
     </div>
   );
 }
