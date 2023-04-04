e34e8e31babd918be5465430131c1f3e8568420f
Kevin Maykel <kevinmaykelgyn@gmail.com>
2023-04-04 08:43:11 -0300

    estudo de parametros

--- Js/Test/index.js
+++ Js/Test/index.js
@@ -14,8 +14,9 @@ app.get("/blog", function (req, res) {
     res.send("Bem vindo a pagina blog!");
 });
 
-
-app.listen(8030, function() {
-    console.log("rodando na url http://localhost:8030");
+app.get("/ola/:cargo/:nome", function (req, res) {
+    res.send("<h1>Ola" + " " + req.params.nome + "</h1>" + "<h2> Seu cargo e: " + req.params.cargo);
 });
+
+app.listen(8030, function() {console.log("rodando na url http://localhost:8030");});
 // localhost:8030
