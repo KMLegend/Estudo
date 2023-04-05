941544281141468b9cd39626dc4cd67c37ed1cf7
Kevin Maykel <kevinmaykelgyn@gmail.com>
2023-04-05 11:22:53 -0300

    coloquei dados pelo sequilize no mysql

---
+++ Js/Test/teste.js
@@ -0,0 +1,62 @@
+import Sequelize from 'sequelize';
+
+const sequelize = new Sequelize('test', 'root', 'Ke12345678901', {
+    host: "localhost",
+    dialect: 'mysql'
+});
+
+
+sequelize.authenticate().then(function () {
+    console.log("Conectado com sucesso!");
+}).catch(function (erro) {
+    console.log("Falha ao se conectar: " + erro);
+})
+
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
+});
+
+/*
+Postagem.create({
+    titulo: "UM Titulo qualquer",
+    conteudo: "123321 tudo sobre 2"
+})
+
+Insere na tabela.
+*/
+
+
+
+
+
+
+
+
+const Pagamentos = sequelize.define('pagamentos', {
+    conta: {
+        type: Sequelize.INTEGER
+    },
+    nome: {
+        type: Sequelize.STRING
+    },
+    nsu: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    valor: {
+        type: Sequelize.DECIMAL
+    },
+    data_vencimento: {
+        type: Sequelize.DATE
+    },
+    data_hora_pagamento: {
+        type: Sequelize.DATE
+    }
+});
+
+
--- Js/Test/teste.sql
+++ Js/Test/teste.sql
@@ -1,5 +1,25 @@
+mysql -h localhost -u root -p
+
+SHOW DATABASES;
+
+USE sistemadecadastro;
+
+SHOW TABLE;
+
 CREATE TABLE usuarios (
     nome VARCHAR(50),
     email VARCHAR(100),
     idade INT
 );
+
+DESCRIBE usuarios;
+
+INSERT INTO usuarios (nome, email, idade) VALUES (
+    "Ana Clara", 
+    "ana@teste.com",
+    8
+);
+
+
+SELECT * FROM usuarios WHERE nome = "Kevin Maykel";
+SELECT * FROM usuarios WHERE idade >= 18;
