e6502685b6b27e737cd3535d40bb8882767479d6
Kevin Maykel <kevinmaykelgyn@gmail.com>
2023-04-05 07:24:23 -0300

    Criação de um banco de dados mysql

--- Js/Test/app.js
+++ Js/Test/app.js
@@ -1,8 +1,27 @@
-import http from 'http';
+import express from "express";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-http.createServer(function(req, res){
-    res.end("Ola, mundo!");
-}).listen(8083);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const app = express();
 
 
-console.log("Server iniciado!");
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/html/index.html");
+});
+
+app.get("/sobre", function (req, res) {
+    res.sendFile(__dirname + "/html/sobre.html");
+});
+
+app.get("/blog", function (req, res) {
+    res.send("Bem vindo a pagina blog!");
+});
+
+app.get("/ola/:cargo/:nome", function (req, res) {
+    res.send("<h1>Ola" + " " + req.params.nome + "</h1>" + "<h2> Seu cargo e: " + req.params.cargo);
+});
+
+app.listen(8030, function() {console.log("rodando na url http://localhost:8030");});
+// localhost:8030
--- Js/Test/index.js
+++ Js/Test/index.js
@@ -1,22 +1,8 @@
-import express from "express";
-const app = express();
+import http from 'http';
+
+http.createServer(function(req, res){
+    res.end("Ola, mundo!");
+}).listen(8083);
 
 
-app.get("/", function (req, res) {
-    res.send("Seja bem vindo!");
-});
-
-app.get("/sobre", function (req, res) {
-    res.send("Minha pagina sobre!");
-});
-
-app.get("/blog", function (req, res) {
-    res.send("Bem vindo a pagina blog!");
-});
-
-app.get("/ola/:cargo/:nome", function (req, res) {
-    res.send("<h1>Ola" + " " + req.params.nome + "</h1>" + "<h2> Seu cargo e: " + req.params.cargo);
-});
-
-app.listen(8030, function() {console.log("rodando na url http://localhost:8030");});
-// localhost:8030
+console.log("Server iniciado!");
